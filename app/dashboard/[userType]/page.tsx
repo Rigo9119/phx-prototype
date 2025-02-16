1a832bdfa719b0ec7b04462a4901e44c3ba17f44
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-02-15 19:44:10 -0500

    fix 2

--- app/dashboard/[userType]/page.tsx
+++ app/dashboard/[userType]/page.tsx
@@ -2,33 +2,34 @@ import CreateProyectSheet from "@/components/sheets/createProyectSheet/createPro
 import { paymentFeeColumns } from "./components/table/columns";
 import DataTable from "@/components/dataTable/dataTable";
 
+export default async function Dashboard({
+	params,
+}: { params: Promise<{ userType: string }> }) {
+	const { userType } = await params;
 
-export default async function Dashboard({ params }: { params: { userType: string}}) {
-  const { userType } = await params;
-
-  return (
-    <div>
-      <div className="flex flex-item items-center justify-between w-full">
-        <div>
-          <h2 className="text-3xl font-bold">Bienvenido {userType}</h2>
-        </div>
-        <div>
-          <CreateProyectSheet triggerLabel="¿Qué tal un prestamo?"/>
-        </div>
-      </div>
-      <section className="mt-8">
-        <h2 className="text-2xl font-bold pb-4">Proyectos</h2>  
-      </section>
-      <section className="mt-8">
-        <h2 className="text-2xl font-bold pb-4">Nivel de deuda</h2>
-      </section>
-      <section className="mt-8">
-        <h2 className="text-2xl font-bold pb-4">Cuotas</h2>
-        <DataTable columns={paymentFeeColumns} data={[]} />
-      </section>
-      <section className="mt-8">
-        <h2 className="text-2xl font-bold pb-4">Estados de cuenta</h2>
-      </section>
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex flex-item items-center justify-between w-full">
+				<div>
+					<h2 className="text-3xl font-bold">Bienvenido {userType}</h2>
+				</div>
+				<div>
+					<CreateProyectSheet triggerLabel="¿Qué tal un prestamo?" />
+				</div>
+			</div>
+			<section className="mt-8">
+				<h2 className="text-2xl font-bold pb-4">Proyectos</h2>
+			</section>
+			<section className="mt-8">
+				<h2 className="text-2xl font-bold pb-4">Nivel de deuda</h2>
+			</section>
+			<section className="mt-8">
+				<h2 className="text-2xl font-bold pb-4">Cuotas</h2>
+				<DataTable columns={paymentFeeColumns} data={[]} />
+			</section>
+			<section className="mt-8">
+				<h2 className="text-2xl font-bold pb-4">Estados de cuenta</h2>
+			</section>
+		</div>
+	);
 }
